17b64efee8b5b934ac1b96118446d084488a17bf
Gábor Dávid <46353496+gAbordAvid@users.noreply.github.com>
2020-10-15 12:34:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function numConverter (num,res) {
     
-    let wordOfNums = num;
-  
-    document.getElementById(res).innerHTML = wordOfNums;
+  const oneToNineTeen = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+  const tenToNinety = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+  const bigNums = ['', 'thousand', 'million', 'billion', 'trillion'];
+  let wordOfNums = '';
+  for (let i = 0; i < bigNums.length; i++) {
+    let x = num%(100*Math.pow(1000,i));
+    console.log(x) //getting the last two numbers
+    
+    
+  }
+document.getElementById(res).innerHTML = wordOfNums;
       
   }
