51ec78f01f2c779cb22abda2d3e38ff6cc268007
Gábor Dávid <46353496+gAbordAvid@users.noreply.github.com>
2020-10-16 10:52:08 +0200

    Displaying everything except hundreds

--- app.js
+++ app.js
@@ -6,10 +6,30 @@ function numConverter (num,res) {
   let wordOfNums = '';
   for (let i = 0; i < bigNums.length; i++) {
     let x = num%(100*Math.pow(1000,i));
-    console.log(x) //getting the last two numbers
     
+    //getting everything except 'hundreds' 
+    if (Math.floor(x/Math.pow(1000,i)) !== 0) {
+
+      //if input number is below 20 (oneToNineTeen)   1 => 19
+      if (Math.floor(x/Math.pow(1000,i)) < 20) {
+       wordOfNums = oneToNineTeen[Math.floor(x/Math.pow(1000,i))] + bigNums[i] + ' ' +wordOfNums;
+      
+      }
+      /*if input is 20 or above 20 BUT this not includes the word "hundred"
+        123456 => twenty-three thousand fifty-six we dont have hundreds yet
+
+        looking backwards
+      */
+      else {
+       wordOfNums = tenToNinety[Math.floor(x/(10*Math.pow(1000,i)))] + '-' + oneToNineTeen[Math.floor(x/Math.pow(1000,i))%10] + bigNums[i] + ' ' +wordOfNums;
+    
+      }
+    }
     
   }
 document.getElementById(res).innerHTML = wordOfNums;
       
   }
+
+      
+  
